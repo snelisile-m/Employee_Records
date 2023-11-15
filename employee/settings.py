93091c326d5f70fb8f9010f46bb1dde14316a140
snelisile-m <snemakha@outlook.com>
2023-11-15 07:47:32 +0200

    procfile

--- employee/settings.py
+++ employee/settings.py
@@ -16,7 +16,6 @@ import django_heroku
 import environ
 import dj_database_url
 
-PORT = int(os.environ.get('PORT', 8000))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
